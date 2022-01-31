291030a1f8d810ec25fe652fa601d3ccabaec91a
Brian Harianja <standinshd@Daniels-MacBook-Air.local>
2022-01-31 18:52:51 +0700

    update

--- routes/webhook.js
+++ routes/webhook.js
@@ -5,7 +5,7 @@ const app = express();
 app.post('/webhook', (req, res) => {
     // Parse the request body from the POST
   let body = req.body;
-
+    console.log(body)
   // Check the webhook event is from a Page subscription
   if (body.object === 'page') {
 
@@ -17,7 +17,7 @@ app.post('/webhook', (req, res) => {
       // Gets the body of the webhook event
         let webhook_event = entry.messaging[0];
         console.log(webhook_event);
-            
+
         // Get the sender PSID
         let sender_psid = webhook_event.sender.id;
         console.log('Sender PSID: ' + sender_psid);
