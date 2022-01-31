b3cbd955c24d2414d3a16634dbd7ea13696983c2
Brian Harianja <standinshd@Daniels-MacBook-Air.local>
2022-01-31 21:26:31 +0700

    update

--- routes/webhook.js
+++ routes/webhook.js
@@ -12,7 +12,7 @@ app.post('/webhook', (req, res) => {
     // Iterate over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
         // Gets the body of the webhook event
-        // let webhook_event = entry.messaging[0];
+        let webhook_event = entry.messaging[0];
         // console.log(webhook_event);
 
         // Get the sender PSID
