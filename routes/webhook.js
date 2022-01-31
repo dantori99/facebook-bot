bbe6740b56bc3f16919a44da20e2197f9cf5d2a1
Brian Harianja <standinshd@Daniels-MacBook-Air.local>
2022-01-31 18:41:48 +0700

    update

--- routes/webhook.js
+++ routes/webhook.js
@@ -3,21 +3,34 @@ const express = require('express');
 const app = express();
 
 app.post('/webhook', (req, res) => {
+    // Parse the request body from the POST
+  let body = req.body;
 
-    let body = req.body;
+  // Check the webhook event is from a Page subscription
+  if (body.object === 'page') {
 
-    if (body.object == 'page') {
+    // Iterate over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
 
-        body.entry.forEach(function(entry) {
+      // Get the webhook event. entry.messaging is an array, but 
+      // will only ever contain one event, so we get index 0
+      // Gets the body of the webhook event
+        let webhook_event = entry.messaging[0];
+        console.log(webhook_event);
+            
+        // Get the sender PSID
+        let sender_psid = webhook_event.sender.id;
+        console.log('Sender PSID: ' + sender_psid);
+      
+    });
 
-            let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
-        });
+    // Return a '200 OK' response to all events
+    res.status(200).send('EVENT_RECEIVED');
 
-        res.status(200).send('EVENT_RECEIVED');
-    } else {
-        res.sendStatus(404);
-    }
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
 })
 
 app.get('/webhook', (req, res) => {
@@ -41,5 +54,20 @@ app.get('/webhook', (req, res) => {
     };
 
 });
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  
+}
 
 module.exports = app;
