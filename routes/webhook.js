1c11ddbbd41b22f35f1551e107f74e657c1bcf43
Brian Harianja <standinshd@Daniels-MacBook-Air.local>
2022-01-31 21:44:00 +0700

    update

--- routes/webhook.js
+++ routes/webhook.js
@@ -8,7 +8,7 @@ app.post('/webhook', (req, res) => {
   let body = req.body;
   // Check the webhook event is from a Page subscription
   if (body.object === 'page') {
-    console.log(body);
+
     // Iterate over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
         // Gets the body of the webhook event
@@ -16,16 +16,16 @@ app.post('/webhook', (req, res) => {
         // console.log(webhook_event);
 
         // Get the sender PSID
-        // let sender_psid = webhook_event;
-        // console.log(sender_psid);
+        let sender_psid = webhook_event.sender.id.first_name;
+        console.log(sender_psid);
         
         // Check if the event is a message or postback and
         // pass the event to the appropriate handler function
-        // if (webhook_event.message) {
-        //   handleMessage(sender_psid, webhook_event.message);        
-        // } else if (webhook_event.postback) {
-        //   handlePostback(sender_psid, webhook_event.postback);
-        // }
+        if (webhook_event.message) {
+          handleMessage(sender_psid, webhook_event.message);        
+        } else if (webhook_event.postback) {
+          handlePostback(sender_psid, webhook_event.postback);
+        }
     });
 
     // Return a '200 OK' response to all events
